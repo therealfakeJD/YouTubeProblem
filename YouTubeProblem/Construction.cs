c6868bc3c983d96a65ee2de5628241c23e4e7032
jdbogieman <justin.diblasi@gmail.com>
2016-03-23 18:01:50 -0500

    Youtube ConStruction Problem

---
+++ YouTubeProblem/Construction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YouTubeProblem
+{
+    class Construction
+    {
+        public Construction()
+        {
+            Process.Start(@"chrome.exe", "https://www.youtube.com/watch?v=hl_9_q_uLF8");
+            Console.ReadLine();
+        }
+
+        //Everything I have read about destructors says they are basically useless.  
+    }
+}
